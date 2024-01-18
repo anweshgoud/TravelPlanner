c9e38d5f1c193c7904c708a80a5bc1df865c2a4b
anweshgoud <anweshgoud2010@gmail.com>
2024-01-18 23:58:03 +0530

    chbginfngf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Project-3-Travel/', 
   plugins: [react()],
-})
+});

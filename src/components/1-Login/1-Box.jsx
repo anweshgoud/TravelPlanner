2ce37903c7faa9fc274409006c28e21ca51d1e04
anweshgoud <anweshgoud2010@gmail.com>
2024-01-20 19:09:44 +0530

    dffdfdfdfdf

--- src/components/1-Login/1-Box.jsx
+++ src/components/1-Login/1-Box.jsx
@@ -1,20 +1,27 @@
+import React from 'react';
+
 const LoginBox = ({ children }) => {
-    return (
-      <div
-        className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5 dialog2 LoginBox"
-        tabindex="-1"
-        role="dialog"
-        id="modalSheet"
-        style={{backgroundImage:`url(LoginTravel.jpg)`}}
-      >
-        <div className="modal-dialog" role="document">
-          <div
-            className="modal-content rounded-4 shadow" style={{width:'35rem',height:'33rem',padding:'10px'}}
-          >{children}</div>
+  return (
+    <div
+      className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5 dialog2 LoginBox"
+      tabIndex="-1"
+      role="dialog"
+      id="modalSheet"
+      style={{ 
+        backgroundImage: 'url(LoginTravel.jpg)',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="modal-dialog" role="document">
+        <div
+          className="modal-content rounded-4 shadow"
+          style={{ width: '35rem', height: '33rem', padding: '10px' }}
+        >
+          {children}
         </div>
       </div>
-    );
-  };
-  
-  export default LoginBox;
-  
+    </div>
+  );
+};
+
+export default LoginBox;
